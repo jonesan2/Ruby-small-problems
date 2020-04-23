dfdb4fe48cc629f9dd068b1570137072bab97f52
Andrew Jones <andrewrayjones@gmail.com>
2020-04-23 14:14:52 +0000

    privacy.rb testing for self; minimax TTT from Ian Eustis

--- exercises/medium_1/privacy.rb
+++ exercises/medium_1/privacy.rb
@@ -2,23 +2,41 @@
 # UNLESS it is a setter method. Then you MUST use self.
 
 class Machine
+  def initialize
+    @switch = :off
+  end
+  
   def start
-    flip_switch(:on)
+    self.switch = :on
   end
 
   def stop
-    flip_switch(:off)
+    self.switch = :off
   end
   
-  def show_off_switch
+  def show_switch
     puts switch
   end
-
+  
+  def pub_flip
+    self.flip_switch
+  end
+  
   private
   
-  attr_accessor :switch
-
-  def flip_switch(desired_state)
-    self.switch = desired_state
+  attr_reader :switch
+  
+  def flip_switch
+    switch == :on ? (self.switch = :off) : (self.switch = :on)
+  end
+  
+  def switch=(desired_state)
+    @switch = desired_state
   end
 end
+
+my_switch = Machine.new
+my_switch.start
+my_switch.show_switch
+my_switch.pub_flip
+my_switch.show_switch
