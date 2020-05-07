541962c8e589ea98de88b1821e595b6740021174
Andrew Jones <andrewrayjones@gmail.com>
2020-05-07 21:53:06 +0000

    Regex

---
+++ regex/fields.rb
@@ -0,0 +1,12 @@
+def fields(str)
+  str.split(/\s*[,\s]\s*/)
+end
+
+p fields("Pete,201,Student")
+# -> ['Pete', '201', 'Student']
+
+p fields("Pete \t 201    ,  TA")
+# -> ['Pete', '201', 'TA']
+
+p fields("Pete \t 201")
+# -> ['Pete', '201']
